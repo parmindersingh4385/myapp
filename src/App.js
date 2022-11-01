2372e57cb54fee82d656fe70414814feada07bae
Parminder <singh1.parminder@gmail.com>
2022-11-01 23:18:52 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<label>PARMINDER SINGH</label>
+			<label>PARMINDER SINGH..........................12345</label>
 		</div>
 	);
 }
